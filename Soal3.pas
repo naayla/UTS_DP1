dcbd06c18f32256344c5870d5188f12590d650f2
naayla <naylasyifa3006@gmail.com>
2024-11-01 11:58:14 +0700

    Update Soal3.pas

--- Soal3.pas
+++ Soal3.pas
@@ -1,1 +1,24 @@
-//ketik kode jawaban noo 3 kamu disini
+program pola;
+var
+  n, i, j: integer;
+
+begin
+  writeln('Masukkan nilai n (harus ganjil): ');
+  readln(n);
+  if n mod 2 = 0 then
+  begin
+    writeln('Angka tidak valid, harus angka ganjil');
+    exit;
+  end;
+  for i := 1 to n do
+  begin
+    for j := 1 to n do
+    begin
+      if (i = j) or (i + j = n + 1) then
+        write('*')
+      else
+        write(' ');
+    end;
+    writeln;
+  end;
+end.
