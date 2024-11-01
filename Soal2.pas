fc02f513618377a69d7c7b04e10bfb7e7343b054
naayla <naylasyifa3006@gmail.com>
2024-11-01 12:01:24 +0700

    Update Soal2.pas

--- Soal2.pas
+++ Soal2.pas
@@ -1,1 +1,27 @@
-//ketik kode jawaban no 2 kamu disini
+program sayur;
+var
+  i, j, jumlah_pelanggan, jumlah_jenis, jumlah_sayur: integer;
+  harga: real; 
+  total_belanja: real; 
+
+begin
+  write('Masukkan jumlah pelanggan: ');
+  readln(jumlah_pelanggan);
+  for i := 1 to jumlah_pelanggan do
+  begin
+    total_belanja := 0; 
+    writeln('Pelanggan ke-', i, ':');
+    write('Masukkan jumlah jenis sayur yang dibeli: ');
+    readln(jumlah_jenis);
+    for j := 1 to jumlah_jenis do
+    begin
+      write('Masukkan harga sayur ke-', j, ': ');
+      readln(harga);
+      
+      write('Masukkan jumlah sayur yang dibeli: ');
+      readln(jumlah_sayur);
+      total_belanja := total_belanja + (harga * jumlah_sayur);
+    end;
+    writeln('Total belanja untuk pelanggan ke-', i, ': Rp ', total_belanja:0:2);
+  end;
+end.
