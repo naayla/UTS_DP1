0e09e65a447cc249667f52f0e5b7984d5873af9c
naayla <naylasyifa3006@gmail.com>
2024-11-02 09:21:10 +0700

    Update Soal 4.pas

--- Soal 4.pas
+++ Soal 4.pas
@@ -1,1 +1,24 @@
-//ketik kode jawaban no 4 kamu disini
+program kembalian;
+const
+  pecahan: array[1..10] of longint = (100000, 75000, 50000, 20000, 10000, 
+                                       5000, 2000, 1000, 500, 200);
+  
+var
+  jumlah_kembalian: longint;
+  jumlah_lembar: integer; 
+  i: integer;
+
+begin
+  write('Masukkan jumlah kembalian: ');
+  readln(jumlah_kembalian);
+  jumlah_lembar := 0;
+  for i := 1 to 10 do
+  begin
+    while jumlah_kembalian >= pecahan[i] do
+    begin
+      jumlah_kembalian := jumlah_kembalian - pecahan[i];
+      jumlah_lembar := jumlah_lembar + 1; 
+    end;
+  end;
+  writeln('Jumlah minimum lembar/koin: ', jumlah_lembar);
+end.
